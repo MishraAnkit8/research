7edc37baddb77f6b0ca8aaef53093cadef5066a6
MishraAnkit8 <Ankit.mishra.ext@nmims.edu>
2024-03-22 15:40:42 +0530

    updated conference presentation

--- src/models/conference-publications.modles.js
+++ src/models/conference-publications.modles.js
@@ -126,103 +126,103 @@ module.exports.updateConferencePublication = async( upadtedConferenceData, confe
     const conferenceFilesarray = {confernceDocString , conferenceProofString}
     console.log('conferenceFiles  =>>', conferenceFilesarray);
 
-    const conferenceFieldToBeUpdate = [
-        { field: 'nmims_campus', value: nmimsCampus },
-        { field: 'nmims_school', value: nmimsSchool },
-        { field: 'title_of_paper', value: titleOfPaper },
-        { field: 'conference_name', value: conferenceName },
-        { field: 'conference_place', value: conferencePlace },
-        {field: 'proceedings_detail' ,value : procedingDetail},
-        { field: 'conference_type', value: conferenceType },
-        { field: 'is_presenter', value: isPresenter },
-        { field: 'organizing_body', value: organizingBody },
-        { field: 'award_for_presentation', value: presentationAward },
-        { field: 'vol_and_issue_no', value: volAndIssueNo },
-        { field: 'issn_isbn_no', value: issnIsbnNo },
-        { field: 'doi_id', value: doiWebLinkId },
-        { field: 'sponsored', value: sponsored },
-        { field: 'spent_amount', value: spentAmount },
-        { field: 'publication_date', value: publicationDate },
-        { field: 'presenting_authors', value: presentingAuthor },
-        { field: 'author_type', value: authorNameString },
-        { field: 'upload_proof', value: conferenceProofString },
-        { field: 'upload_files', value: confernceDocString },
-    ]
+    const doiBookIdParsed = doiWebLinkId === "" ? null : parseInt(doiWebLinkId, 10);
+    const conferenceDocument = confernceDocString || null;
+    const conferenceProofe  = conferenceProofString || null
+    let sql  =  { 
+        text :`UPDATE conference_presentation SET nmims_campus = $2, nmims_school = $3, title_of_paper = $4, conference_name = $5, conference_place = $6, proceedings_detail = $7, conference_type = $8,
+            is_presenter = $9, organizing_body = $10, award_for_presentation = $11, vol_and_issue_no = $12, issn_isbn_no = $13, doi_id = $14,
+            sponsored = $15, spent_amount = $16, publication_date = $17, presenting_authors = $18, author_type = $19, upload_proof = $20, upload_files = $21 WHERE id = $1`,
+        values : [conferenceId, nmimsCampus, nmimsSchool, titleOfPaper, conferenceName, conferencePlace, procedingDetail, conferenceType, isPresenter, organizingBody,
+            presentationAward, volAndIssueNo, issnIsbnNo, doiBookIdParsed, sponsored, spentAmount, publicationDate, presentingAuthor, authorNameString, conferenceProofe, conferenceDocument]
+    }
 
-    console.log('conferenceFieldToBeUpdate ==>>', conferenceFieldToBeUpdate);
-    const setStatements = conferenceFieldToBeUpdate
-        .filter(fieldInfo => fieldInfo.value !== null)
-        .map((fieldInfo, index) => {
-            console.log('dataCondition ===>>>:::::', fieldInfo.value);
-            console.log('index ==>>', index);
-            console.log(`fieldInfo.field ===>>>> ::::: ${fieldInfo.field} = $${index + 2}`)
-            console.log('condition == ==>>>::::', true);
-            return { statement: `${fieldInfo.field} = $${index + 2}`, dataCondition: `${fieldInfo.value}` };
-        });
+    // const conferenceFieldToBeUpdate = [
+    //     { field: 'nmims_campus', value: nmimsCampus },
+    //     { field: 'nmims_school', value: nmimsSchool },
+    //     { field: 'title_of_paper', value: titleOfPaper },
+    //     { field: 'conference_name', value: conferenceName },
+    //     { field: 'conference_place', value: conferencePlace },
+    //     {field: 'proceedings_detail' ,value : procedingDetail},
+    //     { field: 'conference_type', value: conferenceType },
+    //     { field: 'is_presenter', value: isPresenter },
+    //     { field: 'organizing_body', value: organizingBody },
+    //     { field: 'award_for_presentation', value: presentationAward },
+    //     { field: 'vol_and_issue_no', value: volAndIssueNo },
+    //     { field: 'issn_isbn_no', value: issnIsbnNo },
+    //     { field: 'doi_id', value: doiWebLinkId },
+    //     { field: 'sponsored', value: sponsored },
+    //     { field: 'spent_amount', value: spentAmount },
+    //     { field: 'publication_date', value: publicationDate },
+    //     { field: 'presenting_authors', value: presentingAuthor },
+    //     { field: 'author_type', value: authorNameString },
+    //     { field: 'upload_proof', value: conferenceProofString },
+    //     { field: 'upload_files', value: confernceDocString },
+    // ]
 
-    console.log('setStatements ==>>>', setStatements);
+    // console.log('conferenceFieldToBeUpdate ==>>', conferenceFieldToBeUpdate);
+    // const setStatements = conferenceFieldToBeUpdate
+    //     .filter(fieldInfo => fieldInfo.value !== null)
+    //     .map((fieldInfo, index) => {
+    //         console.log('dataCondition ===>>>:::::', fieldInfo.value);
+    //         console.log('index ==>>', index);
+    //         console.log(`fieldInfo.field ===>>>> ::::: ${fieldInfo.field} = $${index + 2}`)
+    //         console.log('condition == ==>>>::::', true);
+    //         return { statement: `${fieldInfo.field} = $${index + 2}`, dataCondition: `${fieldInfo.value}` };
+    //     });
 
-    const updatedConferenceData = conferenceFieldToBeUpdate.map(fieldInfo => {
-        const condition = fieldInfo.value;
-        if(condition){
-            console.log('condition ==>>::::', condition)
-            console.log(`Condition for ${fieldInfo.field}: ${condition}`);
-        }
-        else{
-            return null
-        }
+    // console.log('setStatements ==>>>', setStatements);
+
+    // const updatedConferenceData = conferenceFieldToBeUpdate.map(fieldInfo => {
+    //     const condition = fieldInfo.value;
+    //     if(condition){
+    //         console.log('condition ==>>::::', condition)
+    //         console.log(`Condition for ${fieldInfo.field}: ${condition}`);
+    //     }
+    //     else{
+    //         return null
+    //     }
         
-        const value =  fieldInfo.value ;
-        if(value){
-            console.log(`Value for ${fieldInfo.field}: ${value}`);
-            return value;
-        }
-    }).filter(value => value !== null);
+    //     const value =  fieldInfo.value ;
+    //     if(value){
+    //         console.log(`Value for ${fieldInfo.field}: ${value}`);
+    //         return value;
+    //     }
+    // }).filter(value => value !== null);
 
-    console.log('updatedConferenceData ====::::>>>', updatedConferenceData)
+    // console.log('updatedConferenceData ====::::>>>', updatedConferenceData)
     
-    const updatedConferenceValues = [
-        conferenceId,
-        ...updatedConferenceData,
-    ];
+    // const updatedConferenceValues = [
+    //     conferenceId,
+    //     ...updatedConferenceData,
+    // ];
 
-    console.log('updatedConferenceValues ==>>>', updatedConferenceValues);
+    // console.log('updatedConferenceValues ==>>>', updatedConferenceValues);
 
-    const setStatementString = setStatements.map((item, index) => {
-        if (item.dataCondition !== 'null') {
-        return `${item.statement}`;
-        } else {
-        return '';
-        }
-    }).filter(Boolean).join(', ');
+    // const setStatementString = setStatements.map((item, index) => {
+    //     if (item.dataCondition !== 'null') {
+    //     return `${item.statement}`;
+    //     } else {
+    //     return '';
+    //     }
+    // }).filter(Boolean).join(', ');
     
-    console.log('setStatementString ==>>>', setStatementString);
+    // console.log('setStatementString ==>>>', setStatementString);
     
-    const conferenceSql = {
-        text: `UPDATE conference_presentation SET ${setStatementString} WHERE id = $1`,
-        values: updatedConferenceValues,
-    };
-    console.log('conferenceSql ==>>', conferenceSql);
+    // const conferenceSql = {
+    //     text: `UPDATE conference_presentation SET ${setStatementString} WHERE id = $1`,
+    //     values: updatedConferenceValues,
+    // };
+    // console.log('conferenceSql ==>>', conferenceSql);
+    
     console.log('externalNamesString ===>>>>', externalNamesString)
     const externalEmpSql = externalNamesString ? {
         text: `INSERT INTO external_emp(external_emp_name) VALUES ($1) RETURNING id`,
         values: [externalNamesString]
     } : null;
 
-    // const conferenceTable = await researchDbW.query(conferenceSql);
-    // const externalEmpTable = externalEmpSql ? await researchDbW.query(externalEmpSql) : null;
-    // const promises = [conferenceTable, externalEmpTable];
-    // return Promise.all(promises).then(([conferenceTable, externalEmpTable]) => {
-    //     console.log('conferenceTable ===>>>', conferenceTable);
-    //     console.log('externalEmpTable ===>>>>', externalEmpTable)
-    //     return {status : 'Done' , message : "Record Updated Successfully" , externalEmpId : externalEmpSql !== null ? externalEmpTable.rows[0].id : null, conferenceTable : conferenceTable}
-    // })
-    // .catch((error) => {
-    //     console.log('error ===>>>', conferenceTable);
-    //     console.log('err.message ===>>>', err.message)
-    //     return{status : "Failed", message : error.message, errorCode : error.code}
-    // })
-    const conferenceTablePromise = researchDbW.query(conferenceSql)
+
+    const conferenceTablePromise = researchDbW.query(sql)
         .then(conferenceTable => {
             return conferenceTable;
         })
