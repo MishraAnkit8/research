d1850e51a4a1965c0e2504152e1527bf26f8773b
MishraAnkit8 <Ankit.mishra.ext@nmims.edu>
2024-04-30 12:12:56 +0530

    multer file limit extend

--- multer.js
+++ multer.js
@@ -1,21 +1,21 @@
 const { v4: uuidv4 } = require('uuid');
 const multer = require('multer');
 
- // Set up multer for handling file uploads
+// Set up multer for handling file uploads
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'uploads/'); // destination folder
     },
     filename: function (req, file, cb) {
-
-        console.log('file in multer ==>> ::::', file);
-        // Define the filename as the original filename
-        // let uniqueFileName = file.originalname + '_' + uuidv4();
-        let uniqueFileName = file.originalname;
+        // Append a unique identifier (UUID) to the original filename
+        let uniqueFileName = uuidv4() + '_' + file.originalname;
         cb(null, uniqueFileName);
     }
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ 
+    storage: storage,
+    limits: { fileSize: 10 * 1024 * 1024 } 
+});
 
-module.exports = upload
+module.exports = upload;
