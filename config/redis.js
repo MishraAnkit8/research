3ccd05b9ddbdc0fd3c594f17082f30da4bb768df
harshalnmims <harshal.nanekar.EXT@nmims.edu>
2024-05-03 19:10:53 +0530

    message

--- config/redis.js
+++ config/redis.js
@@ -1,24 +1,24 @@
-const redis = require('redis');
+const redis = require("redis");
 
 // Redis client configuration
 const redisClientConn = redis.createClient({
-    host: process.env.REDIS_HOST || 'localhost',
-    port: process.env.REDIS_PORT || 63796,
-    password: process.env.REDIS_PASSWORD || ''
+  host: process.env.REDIS_HOST || "localhost",
+  port: process.env.REDIS_PORT || 63796,
+  password: process.env.REDIS_PASSWORD || "yourpassword",
 });
 
 // redis passWord yourpassword for server
 const redisClient = redisClientConn.connect();
 
 // Optional: Handle Redis client errors
-redisClientConn.on('connect', () => {
-    console.log('Connected to Redis');
+redisClientConn.on("connect", () => {
+  console.log("Connected to Redis");
 });
 
 // Handle Redis client errors
-redisClientConn.on('error', (err) => {
-    console.log('Redis error: ', err);
-    // internalServerError({ moduleName: 'redis.js', message: 'Redis connection failed!' });
+redisClientConn.on("error", (err) => {
+  console.log("Redis error: ", err);
+  // internalServerError({ moduleName: 'redis.js', message: 'Redis connection failed!' });
 });
 
-module.exports = { redisClient }
+module.exports = { redisClient };
