21f4fdb95852ec7618c6bbde31a71aefa2a63bd7
harshalnmims <harshal.nanekar.EXT@nmims.edu>
2024-05-11 18:59:24 +0530

    commit

--- config/redis.js
+++ config/redis.js
@@ -1,11 +1,10 @@
-
 const redis = require("redis");
 
 // Redis client configuration
 const redisClientConn = redis.createClient({
   host: process.env.REDIS_HOST || "localhost",
   port: process.env.REDIS_PORT || 63796,
-  password: process.env.REDIS_PASSWORD || "",
+  password: process.env.REDIS_PASSWORD || "yourpassword",
 });
 
 // redis passWord yourpassword for server
