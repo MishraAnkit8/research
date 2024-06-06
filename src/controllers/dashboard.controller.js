c2a1135082d350bcd0d5f3e8110da0efbe5ef434
MishraAnkit8 <Ankit.mishra.ext@nmims.edu>
2024-06-06 12:08:10 +0530

    commit

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,4 +1,14 @@
+
+const { getRedisData } = require('../../utils/redis.utils');
+
 module.exports.renderDashboard = async (req, res, next) => {
-    res.render('dashboard');
+    const sessionid = req.cookies.session;
+    let sessionData = await getRedisData(`${sessionid}:session`)
+    const  userName = sessionData.username;
+    console.log('userName in in dashboard controller  ===>>>>>>', userName);
+
+    res.render('dashboard' , {
+        userName : userName
+    });
 };
 
