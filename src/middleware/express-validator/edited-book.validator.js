a21680f6dd8f212ad172a52d802ed1cad13c569e
MishraAnkit8 <Ankit.mishra.ext@nmims.edu>
2024-06-10 18:30:34 +0530

    commit

--- src/middleware/express-validator/edited-book.validator.js
+++ src/middleware/express-validator/edited-book.validator.js
@@ -1,160 +1,160 @@
-const { check } = require("express-validator");
-const { validationHandler } = require("./validation.handler");
+// const { check } = require("express-validator");
+// const { validationHandler } = require("./validation.handler");
 
-//check if value is an integer
-const isInt = (value) => {
-  if (value && isNaN(value)) {
-    return false;
-  }
-  return true;
-};
+// //check if value is an integer
+// const isInt = (value) => {
+//   if (value && isNaN(value)) {
+//     return false;
+//   }
+//   return true;
+// };
 
-// Custom validator to check if the stringified array contains valid IDs
-const isStringifiedArray = (value) => {
-    try {
-      const parsed = JSON.parse(value);
-      if (parsed && Object.values(parsed).length === 1) {
-        const ids = Object.values(parsed)[0];
-        return Array.isArray(ids) && ids.every(id => typeof id === 'string' || typeof id === 'number');
-      }
-      return false;
-    } catch (e) {
-      return false;
-    }
-  };
+// // Custom validator to check if the stringified array contains valid IDs
+// const isStringifiedArray = (value) => {
+//     try {
+//       const parsed = JSON.parse(value);
+//       if (parsed && Object.values(parsed).length === 1) {
+//         const ids = Object.values(parsed)[0];
+//         return Array.isArray(ids) && ids.every(id => typeof id === 'string' || typeof id === 'number');
+//       }
+//       return false;
+//     } catch (e) {
+//       return false;
+//     }
+//   };
 
 
-//   cunstom validation for file 
-const isValidFile = (files) => {
-    if (!files || files.length === 0) {
-      return false; 
-    }
+// //   cunstom validation for file 
+// const isValidFile = (files) => {
+//     if (!files || files.length === 0) {
+//       return false; 
+//     }
   
-    const allowedTypes = ["application/pdf", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "application/vnd.ms-excel.sheet.macroenabled.12", "application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"];
-    const maxSizeInBytes = 5 * 1024 * 1024; // 5MB
+//     const allowedTypes = ["application/pdf", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "application/vnd.ms-excel.sheet.macroenabled.12", "application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"];
+//     const maxSizeInBytes = 5 * 1024 * 1024; // 5MB
   
-    for (const file of files) {
-      if (!allowedTypes.includes(file.mimetype)) {
-        return false; 
-      }
+//     for (const file of files) {
+//       if (!allowedTypes.includes(file.mimetype)) {
+//         return false; 
+//       }
   
-      if (file.size > maxSizeInBytes) {
-        return false; 
-      }
-    }
+//       if (file.size > maxSizeInBytes) {
+//         return false; 
+//       }
+//     }
   
-    return true;
-  };
+//     return true;
+//   };
   
 
 
-// validate org insert
-module.exports.validateEditedBook = [
-check('authorName')
-    .notEmpty()
-    .withMessage('All authors names is required')
-    .bail()
-    .isString()
-    .withMessage('All authors names should be a string')
-    .isLength({ min: 2 }),
+// // validate org insert
+// module.exports.validateEditedBook = [
+// check('authorName')
+//     .notEmpty()
+//     .withMessage('All authors names is required')
+//     .bail()
+//     .isString()
+//     .withMessage('All authors names should be a string')
+//     .isLength({ min: 2 }),
 
-check('nmimsSchoolAuthors')
-    .notEmpty()
-    .withMessage('NMIMS School - Author  is required')
-    .bail()
-    .isString()
-    .withMessage('NMIMS School - Author should be a string')
-    .isLength({ min: 2 }),
+// check('nmimsSchoolAuthors')
+//     .notEmpty()
+//     .withMessage('NMIMS School - Author  is required')
+//     .bail()
+//     .isString()
+//     .withMessage('NMIMS School - Author should be a string')
+//     .isLength({ min: 2 }),
 
-check('nmimsCampusAuthors')
-    .notEmpty()
-    .withMessage('NMIMS Campus - Author  is required')
-    .bail()
-    .isString()
-    .withMessage('NMIMS Campus - Author  should be a string')
-    .isLength({ min: 2 }),
+// check('nmimsCampusAuthors')
+//     .notEmpty()
+//     .withMessage('NMIMS Campus - Author  is required')
+//     .bail()
+//     .isString()
+//     .withMessage('NMIMS Campus - Author  should be a string')
+//     .isLength({ min: 2 }),
 
-check('bookTitle')
-    .notEmpty()
-    .withMessage('Title Of Book  is required')
-    .bail()
-    .isString()
-    .withMessage('Title Of Book  should be a string')
-    .isLength({ min: 2 }),
+// check('bookTitle')
+//     .notEmpty()
+//     .withMessage('Title Of Book  is required')
+//     .bail()
+//     .isString()
+//     .withMessage('Title Of Book  should be a string')
+//     .isLength({ min: 2 }),
 
- check('edition')
-    .notEmpty()
-    .withMessage('Edition is required')
-    .bail()
-    .isAlphanumeric()
-    .withMessage('Edition should be combination of alphanumeric '),
+//  check('edition')
+//     .notEmpty()
+//     .withMessage('Edition is required')
+//     .bail()
+//     .isAlphanumeric()
+//     .withMessage('Edition should be combination of alphanumeric '),
 
-check('publisherName')
-    .notEmpty()
-    .withMessage('Publisher Name is required')
-    .bail()
-    .isString()
-    .withMessage('Publisher Name should be a string'),
+// check('publisherName')
+//     .notEmpty()
+//     .withMessage('Publisher Name is required')
+//     .bail()
+//     .isString()
+//     .withMessage('Publisher Name should be a string'),
 
-check('publisherCategory')
-    .notEmpty()
-    .withMessage('Publisher Category required')
-    .bail()
-    .isString()
-    .withMessage('Publisher Category should be a string'),
+// check('publisherCategory')
+//     .notEmpty()
+//     .withMessage('Publisher Category required')
+//     .bail()
+//     .isString()
+//     .withMessage('Publisher Category should be a string'),
 
-check('publicationYear')
-    .notEmpty()
-    .withMessage('Publication Year is required')
-    .bail()
-    .isInt({ min: 1900, max: 3000 })
-    .withMessage('Publication Year should be an integer between 1900 to 3000'),
-
-
-
-check('isbnNo')
-    .notEmpty()
-    .withMessage('ISBN Number is required')
-    .bail()
-    .isString()
-    .withMessage('ISBN Number should be astring'),
-
-check('doiBookId')
-    .notEmpty()
-    .withMessage('DOI ID/ WebLink is required')
-    .bail()
-    .isString()
-    .withMessage('DOI ID/ WebLink should be a string'),
-
-check('publicationPlace')
-    .notEmpty()
-    .withMessage('Place Of Publication required')
-    .bail()
-    .isString()
-    .withMessage('Place Of Publication should be a string'),
-
-check('editorName')
-    .notEmpty()
-    .withMessage('Editor(s) Of The Book required')
-    .bail()
-    .isString()
-    .withMessage('Editor(s) Of The Book should be a string'),
+// check('publicationYear')
+//     .notEmpty()
+//     .withMessage('Publication Year is required')
+//     .bail()
+//     .isInt({ min: 1900, max: 3000 })
+//     .withMessage('Publication Year should be an integer between 1900 to 3000'),
 
 
-check('numberOfNmimsAuthors')
-    .notEmpty()
-    .withMessage('No. Of NMIMS Authors is required')
-    .bail()
-    .isInt()
-    .withMessage('No. Of NMIMS Authors should be an integer'),
 
-check('nmimsAuthors')
-    .notEmpty()
-    .withMessage('Name of NMIMS Authors is required')
-    .bail()
-    .isString()
-    .withMessage('Name of NMIMS Authors should be a string')
-    .isLength({ min: 2 }),
+// check('isbnNo')
+//     .notEmpty()
+//     .withMessage('ISBN Number is required')
+//     .bail()
+//     .isString()
+//     .withMessage('ISBN Number should be astring'),
 
-  validationHandler, // Handler for validation errors
-];
+// check('doiBookId')
+//     .notEmpty()
+//     .withMessage('DOI ID/ WebLink is required')
+//     .bail()
+//     .isString()
+//     .withMessage('DOI ID/ WebLink should be a string'),
+
+// check('publicationPlace')
+//     .notEmpty()
+//     .withMessage('Place Of Publication required')
+//     .bail()
+//     .isString()
+//     .withMessage('Place Of Publication should be a string'),
+
+// check('editorName')
+//     .notEmpty()
+//     .withMessage('Editor(s) Of The Book required')
+//     .bail()
+//     .isString()
+//     .withMessage('Editor(s) Of The Book should be a string'),
+
+
+// check('numberOfNmimsAuthors')
+//     .notEmpty()
+//     .withMessage('No. Of NMIMS Authors is required')
+//     .bail()
+//     .isInt()
+//     .withMessage('No. Of NMIMS Authors should be an integer'),
+
+// check('nmimsAuthors')
+//     .notEmpty()
+//     .withMessage('Name of NMIMS Authors is required')
+//     .bail()
+//     .isString()
+//     .withMessage('Name of NMIMS Authors should be a string')
+//     .isLength({ min: 2 }),
+
+//   validationHandler, // Handler for validation errors
+// ];
